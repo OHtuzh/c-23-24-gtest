84f86fec266530b4c4c00c369b4d83dc56f16469
tema-m <aymoiseenko3@gmail.com>
2024-02-25 17:09:45 +0300

    new topics

--- test/algorithm_ut.cpp
+++ test/algorithm_ut.cpp
@@ -1,8 +1,11 @@
 #include "lib/algorithm.hpp"
 
+#include "gtest/gtest.h"
 #include <gtest/gtest.h>
 
+#include <tuple>
 #include <vector>
+#include <list>
 #include <string>
 
 struct IsEven {
@@ -64,4 +67,75 @@ TEST(ACCUMULATE, CONCAT_ONLY_SMALL_STRINGS) {
     };
     const std::string kInitString = "init";
     ASSERT_EQ(ohtuzh::accumulate(v.begin(), v.end(), kInitString, ConcatOnlySmallStringsOperator{}), kInitString + "small1small2small3");
+}
+
+class AccumulateIntTest : public ::testing::TestWithParam<std::tuple<std::vector<int>, int, int>> {};
+
+TEST_P(AccumulateIntTest, SimpleTest) {
+    const std::vector<int>& container = std::get<0>(GetParam());
+    int init = std::get<1>(GetParam());
+    int result = std::get<2>(GetParam());
+    ASSERT_EQ(ohtuzh::accumulate(container.begin(), container.end(), init), result);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ACCUMULATE_SIMPLE,
+    AccumulateIntTest,
+    ::testing::Values(
+        std::make_tuple(std::vector<int>{1, 2, 3, 4, 5}, 0, 15),
+        std::make_tuple(std::vector<int>{1, 2, 3, 4, 5}, 10, 25),
+        std::make_tuple(std::vector<int>{1, -1, 1, -1, 1}, -1, 0)
+    )
+);
+
+class AccumulateStringTest : public ::testing::TestWithParam<std::tuple<std::vector<std::string>, std::string, std::string>> {};
+
+TEST_P(AccumulateStringTest, SimpleTest) {
+    const std::vector<std::string>& container = std::get<0>(GetParam());
+    std::string init = std::get<1>(GetParam());
+    std::string result = std::get<2>(GetParam());
+    ASSERT_EQ(ohtuzh::accumulate(container.begin(), container.end(), init), result);
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ACCUMULATE_SIMPLE,
+    AccumulateStringTest,
+    ::testing::Values(
+        std::make_tuple(std::vector<std::string>{"Hello", ", world!"}, "", "Hello, world!"),
+        std::make_tuple(std::vector<std::string>{"Hello", ", world!", " Qwe."}, "", "Hello, world! Qwe.")
+    )
+);
+
+// Via template:
+template<typename T>
+using AccumulateAnyTestParams = std::vector<std::tuple<std::vector<T>, T, T>>;
+
+const std::tuple<AccumulateAnyTestParams<int>, AccumulateAnyTestParams<std::string>> AllAccumulateAnyTestParams = {
+    {
+        std::make_tuple(std::vector<int>{1, 2, 3, 4, 5}, 0, 15),
+        std::make_tuple(std::vector<int>{1, 2, 3, 4, 5}, 10, 25),
+        std::make_tuple(std::vector<int>{1, -1, 1, -1, 1}, -1, 0)
+    },
+    {
+        std::make_tuple(std::vector<std::string>{"Hello", ", world!"}, "", "Hello, world!"),
+        std::make_tuple(std::vector<std::string>{"Hello", ", world!", " Qwe."}, "", "Hello, world! Qwe.")
+    }
+};
+using AccumulateAnyTestTypes = ::testing::Types<int, std::string>;
+
+template<typename T>
+class AccumulateAnyTest : public ::testing::Test {
+public:
+    AccumulateAnyTest() : params(std::get<AccumulateAnyTestParams<T>>(AllAccumulateAnyTestParams)) {}
+protected:
+    AccumulateAnyTestParams<T> params;
+};
+
+TYPED_TEST_SUITE(AccumulateAnyTest, AccumulateAnyTestTypes);
+
+TYPED_TEST(AccumulateAnyTest, CommonAccumulate) {
+    for (const auto& [container, init, expected]: this->params) {
+        const auto actual = ohtuzh::accumulate(container.begin(), container.end(), init);
+        ASSERT_EQ(actual, expected) << actual << " != " << expected;
+    }
 }
